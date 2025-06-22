70fb57b0daef91b2ea77cd37903a16905680b119
SwarajBhattacharjee <swarajbhattacharjee0@gmail.com>
2025-06-22 23:46:45 +0530

    Created the first question model

---
+++ question_model.py
@@ -0,0 +1,12 @@
+# Create a new class question that has two attributes text and answer
+class Question:
+    
+    def __init__(self,text,answer):
+        self.text = text
+        self.answer = answer
+        
+# test 
+new_q = Question("2+3 = 5", "True")
+print(new_q.text)
+print(new_q.answer)
+
