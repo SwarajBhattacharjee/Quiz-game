fb3a75a6ca2e0d08c87d19419e340f8309dfbdab
SwarajBhattacharjee <swarajbhattacharjee0@gmail.com>
2025-06-23 12:49:12 +0530

    Added the scoring system

--- quiz_brain.py
+++ quiz_brain.py
@@ -4,7 +4,7 @@ class QuizBrain():
     def __init__(self,question_list):
         self.question_number = 0
         self.question_list = question_list
-    
+    user_score = 0
     def next_question(self):
         # first approach
         # user_input = input(f"Q {self.question_number +1}: {self.question_list[self.question_number].text} (True / False) : ")
@@ -12,7 +12,20 @@ class QuizBrain():
         # easier approach
         current_question = self.question_list[self.question_number]
         self.question_number +=1
-        user_input = input(f"Q.{self.question_number}: {current_question.text} (True / False) : ")
+        user_answer = input(f"Q.{self.question_number}: {current_question.text} (True / False) : ")
+        if self.check_answer(user_answer,current_question.answer):
+            self.user_score +=1
+            print("You got it right!")
+            print(f"The correct answer is {current_question.answer}")
+            print(f"Your score is {self.user_score}/{self.question_number}")
+        else:
+            print("You got it wrong")
+            print(f"The correct answer is {current_question.answer}")
+            print(f"Your score is {self.user_score}/{self.question_number}")
+        
+    
+    def check_answer(self,user_answer,correct_answer):
+        return user_answer.lower() == correct_answer.lower()
 
 
 # Checking if the answer was correct
@@ -20,4 +33,3 @@ class QuizBrain():
 # Checking if we are at the end of the quiz
     def still_has_question(self):
         return self.question_number < len(self.question_list)
-        
